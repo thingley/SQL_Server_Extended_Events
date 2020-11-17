3117a05b84fbf35c4b4469b7a2f496f4c2087b3c
Trevor Hingley <Trevor.Hingley@oxfordcc.co.uk>
2020-11-17 11:17:06 +0000

    Corrected duration to match script title

--- Session_ModuleDurationOver20Seconds.sql
+++ Session_ModuleDurationOver20Seconds.sql
@@ -16,7 +16,7 @@ GO
 CREATE EVENT SESSION [ModuleDurationOver20Seconds] ON SERVER 
 ADD EVENT sqlserver.module_end(
     ACTION(package0.callstack,sqlserver.database_name,sqlserver.tsql_stack,sqlserver.username)
-    WHERE ([duration]>(25000000)))
+    WHERE ([duration]>(20000000)))
 ADD TARGET package0.event_file(SET filename=N'D:\TEMP\ModuleDurationOver20Seconds.xel')
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
 GO
